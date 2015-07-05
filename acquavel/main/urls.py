23e6a7910a59424c89478a89618200947e9554bc
ScorpMarv <marv1336@gmail.com>
2015-07-04 23:48:46 -0300

    cleaning solution

---
+++ acquavel/main/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = patterns('',
+   url(r'^socio/(?P<pk>[0-9]+)/$', views.SocioDetail.as_view())
+)
+
+urlpatterns = format_suffix_patterns(urlpatterns)
