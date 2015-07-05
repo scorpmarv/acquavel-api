d6df308e75b4911e8af89e3218e9f1f5c6ec17fa
ScorpMarv <marv1336@gmail.com>
2015-07-05 14:45:56 -0300

    Working serializer and CORS

--- acquavel/acquavel/settings.py
+++ acquavel/acquavel/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'main',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -51,6 +53,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'acquavel.urls'
 
--- acquavel/main/serializers.py
+++ acquavel/main/serializers.py
@@ -4,4 +4,4 @@ from .models import Socio
 class SocioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Socio
-        field = ('id', 'dni')
+        fields = ('dni',)
