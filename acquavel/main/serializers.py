aa03f5b198df8b448defd7f389dd7deb72aab244
ScorpMarv <marv1336@gmail.com>
2015-07-12 03:02:51 -0300

    Filter active plans and show remaining lessons

--- acquavel/main/serializers.py
+++ acquavel/main/serializers.py
@@ -1,11 +1,6 @@
+from datetime import datetime
 from rest_framework import serializers
 from .models import Socio, Plan, Ingreso, Contratacion
-
-
-class IngresoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ingreso
-        fields = ('contratacion', 'fecha')
 
 
 class PlanSerializer(serializers.ModelSerializer):
@@ -17,16 +12,28 @@ class PlanSerializer(serializers.ModelSerializer):
 
 class ContratacionSerializer(serializers.ModelSerializer):
     plan = PlanSerializer(read_only=True)
+    clases_restantes = serializers.SerializerMethodField()
+
+    def get_clases_restantes(self, obj):
+        clases_plan = obj.plan.cant_clases
+        ingresos = Ingreso.objects.filter(contratacion=obj).count()
+        return clases_plan - ingresos
 
     class Meta:
         model = Contratacion
-        fields = ('inicio', 'final', 'plan')
+        fields = ('inicio', 'final', 'plan', 'clases_restantes')
 
 
 class SocioSerializer(serializers.ModelSerializer):
-    planes = ContratacionSerializer(source='contratacion_set', many=True)
+    planes = serializers.SerializerMethodField()
+
+    def get_planes(self, obj):
+        now = datetime.now()
+        contrataciones = Contratacion.objects.filter(socio=obj, inicio__lte=now,
+                                                     final__gte=now)
+        serializer = ContratacionSerializer(contrataciones, many=True)
+        return serializer.data
 
     class Meta:
         model = Socio
         fields = ('dni', 'planes')
-        # fields = ('dni', 'planes')
