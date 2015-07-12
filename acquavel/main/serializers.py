3f724da803ff314b6472a0d759c1ac51074b4195
ScorpMarv <marv1336@gmail.com>
2015-07-12 02:32:29 -0300

    Added planes to SocioSerializer

--- acquavel/main/serializers.py
+++ acquavel/main/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
-from .models import Socio
+from .models import Socio, Plan, Ingreso, Contratacion
+
+
+class IngresoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingreso
+        fields = ('contratacion', 'fecha')
+
+
+class PlanSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Plan
+        fields = ('descripcion', 'cant_clases')
+
+
+class ContratacionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer(read_only=True)
+
+    class Meta:
+        model = Contratacion
+        fields = ('inicio', 'final', 'plan')
+
 
 class SocioSerializer(serializers.ModelSerializer):
+    planes = ContratacionSerializer(source='contratacion_set', many=True)
+
     class Meta:
         model = Socio
-        fields = ('dni',)
+        fields = ('dni', 'planes')
+        # fields = ('dni', 'planes')
